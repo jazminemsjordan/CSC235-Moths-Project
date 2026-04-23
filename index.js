c626acf915db49657c49852583d76129e9cf11da
jazminemsjordan <jazminejordan77@gmail.com>
2026-04-22 22:03:27 -0400

    colored scatterplot

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 */
 
 const data = await d3.csv('mothitor.csv')
-
+console.log(data)
 // changing date format from month abbreviation to numerical
 const parseTime = d3.timeParse("%b %-d %Y");
 
@@ -19,56 +19,43 @@ for (let i = 0; i < data.length; i++) {
 let dateStrings = data.map(d => d.date.toISOString())
 dateStrings = Array.from(new Set((dateStrings)))
 
-// storing counts for each date
-let counts = []
-// (LORELEI) REPLACE BELOW CODE!!
-const grouped = d3.group(data, d => d.date.toISOString())
-/* (LORELEI) ^^^^REPLACE THIS WITH THE BELOW CODE^^^
-    const grouped = d3.group(data,
-    d => d.date.toISOString(),
-    d => d.deployment_id
-);*/
-console.log(grouped)
-console.log(dateStrings)
+//grouping
+const grouped = d3.group(data, 
+    d => d.deployment_name, 
+    d => d.date.toISOString()
+);
 
-for (let i = 0; i < dateStrings.length; i++) {
-    counts.push(grouped.get(dateStrings[i]).length)
-}
-
-// turning strings back to date format
+/* get counts for each station 
+How this works: if statement checks to make sure that all three groups have data for a given date, to keep our comparisons fair
+One array is created for each station. The date is converted from a string to a date object and added to the first column of all three arrays.
+(dates are also added to their own array for the domain of our x axis later)
+The count is recorded for each group and each date in their respective arrays
+The name of each station is recorded in every object of the array
+*/
+let syd = []
+let ama = []
+let car = []
 let dates = []
 for (let i = 0; i < dateStrings.length; i++) {
-    dates.push(new Date(dateStrings[i]));
-}
-
-// sorting for troubleshooting
-const sortedDates = dates.sort((a, b) => a - b);
-console.log(sortedDates);
-
-// creating data array for scatterplot
-// (LORELEI) SCATTERPLOT ARRAY NEEDS TO BE EDITED TO INCLUDE DEPLOYMENT_ID, 
-const scatterData = counts.map((val, index) => [val, dates[index]]);
-/* I'M NOT SURE ENTIRELY HOW TO GO ABOUT THIS. BELOW IS THE SUGGESTED METHOD BY VS CODE ON MY CODE ATTEMPT AT IT THAT DIDNT WORK. 
-I WOULD NOT SUGGEST BLINDLY TRUSTING IT
-
-const scatterData = [];
-for (const [dateStr, deployments] of grouped) {
-    for (const [deploymentId, observations] of deployments) {
-        scatterData.push({
-            count: observations.length,
-            date: new Date(dateStr),
-            deployment_id: deploymentId
-        });
+    if (grouped.get("SYD").get(dateStrings[i]) != undefined && grouped.get("AMA").get(dateStrings[i]) != undefined && grouped.get("CAR").get(dateStrings[i]) != undefined){
+        let date = new Date(dateStrings[i])
+        dates.push(date)
+        syd.push([date, grouped.get("SYD").get(dateStrings[i]).length, "SYD"])
+        ama.push([date, grouped.get("AMA").get(dateStrings[i]).length, "AMA"])
+        car.push([date, grouped.get("CAR").get(dateStrings[i]).length, "CAR"])
     }
-} */
+};
 
-/* (LORELEI) COLORS FOR SCATTERPLOT POINTS SHOULD BE COLORBLIND FRIENDLY!!
-    ALSO WE SHOULD BE EASILY ABLE TO CHANGE TO DEPLOYMENT_NAME SINCE I REALIZED HERE WE ARE USING THE ID*/
-/*const colorMap = {
-    "296": "black",
-    "297": "blue", 
-    "325": "orange"
-};*/
+// Builds the scatterplot data array by stacking the three station arrays
+const scatterData = syd.concat(ama, car)
+console.log(scatterData)
+
+// (LORELEI) COLORS FOR SCATTERPLOT POINTS SHOULD BE COLORBLIND FRIENDLY!! 
+const colorMap = {
+    "SYD": "black",
+    "AMA": "blue", 
+    "CAR": "orange"
+};
 
 // x axis
 const x = d3.scaleTime()
@@ -90,7 +77,6 @@ d3.select('#yaxis')
     .attr("transform", "translate(40, 0)")
 
 
-
 // plot scatterplot points
 d3.select('#points')
     .selectAll("dot")
@@ -99,14 +85,12 @@ d3.select('#points')
     .append("circle")
         /* (LORELEI) WHY CANT IT JUST BE ".attr("cx", d => x(d.date))" AND ".attr( "cy", d => y(d.count))"
          FOR THE BELOW CODE? */
-        .attr("cx", function (d) { 
-            console.log(x(d[1]))
-            return x(d[1]); 
-        } )
-        .attr("cy", function (d) {
-            return y(d[0]); 
-        } )
+        /* answer: we can do the pointers like that! I switched it. The function line was a bit unwieldy, you're right.
+        we can't use .date and .count because they don't exist in scatterdata. it's a new, basic array. it doesn't have named columns, so we need to index numerically
+        the data index read from the csv creates an array of objects, which each have a key (column name) and a value (actual data). scatterData doesn't have that functionality.
+        */
+        .attr("cx", d => x(d[0]))
+        .attr("cy", d => y(d[1]))
         .attr("r", 3)
-        // (LORELEI) CODE BELOW NEEDS TO BE ADDED TO HAVE COLOR BE BY DEPLOYMENT_ID
-        /*.style("fill", d => colorMap[d.deployment_id]); */
+        .style("fill", d => colorMap[d[2]]); 
 
